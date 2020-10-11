acba880a81ac6544e4c5581275b6bd4eb3286644
mtyszler <tyszler.jobs@gmail.com>
2020-10-11 08:08:55 +0200

    Removed unused import statements

--- app/run.py
+++ app/run.py
@@ -2,13 +2,9 @@ import json
 import plotly
 import pandas as pd
 
-from nltk.stem import WordNetLemmatizer
-from nltk.tokenize import word_tokenize
-
 from flask import Flask
 from flask import render_template, request, jsonify
 from plotly.graph_objs import Bar
-#from sklearn.externals import joblib
 import joblib
 from sqlalchemy import create_engine
 
